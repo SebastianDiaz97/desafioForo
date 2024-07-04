d73368f94fe5a06b4c5a17bb8c3a9e967ab4633e
SebastianDiaz97 <sdiazr1997@gmail.com>
2024-07-04 17:37:19 -0400

    first commit

---
+++ src/test/java/com/example/DesafioForo/DesafioForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DesafioForo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
